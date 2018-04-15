53d32b2442c13f34a06a3f19370a9eebf369be2e
Sergio Llopis <sllopis@csumb.edu>
2018-04-15 07:32:31 +0000

    Regrade

--- labs/lab6/index.php
+++ labs/lab6/index.php
@@ -24,46 +24,65 @@
     }
     
     function displaySearchResults(){
-        global $conn;
-        
-        if (isset($_GET['searchForm'])) { //checks whether user has submitted the form
-            
-            echo "<h3>Products Found: </h3>"; 
-            
-            //following sql works but it DOES NOT prevent SQL Injection
-            //$sql = "SELECT * FROM om_product WHERE 1
-            //       AND productName LIKE '%".$_GET['product']."%'";
-            
-            //Query below prevents SQL Injection
-            
-            $namedParameters = array();
-            
-            $sql = "SELECT * FROM om_product WHERE 1";
-            
-            if (!empty($_GET['product'])) { //checks whether user has typed something in the "Product" text box
-                 $sql .=  " AND productName LIKE :productName";
-                 $namedParameters[":productName"] = "%" . $_GET['product'] . "%";
-            }
-                  
-                  
+         global $conn;
+         
+         if (isset($_GET['searchForm'])) { //checks whether user has submitted the form
+             
+             echo "<h1>Products Found: </h1>"; 
+             
+             //following sql works but it DOES NOT prevent SQL Injection
+             //$sql = "SELECT * FROM om_product WHERE 1
+             //       AND productName LIKE '%".$_GET['product']."%'";
+             
+             //Query below prevents SQL Injection
+             
+             $namedParameters = array();
+             
+             $sql = "SELECT * FROM om_product WHERE 1";
+             
+             if (!empty($_GET['product'])) { //checks whether user has typed something in the "Product" text box
+                  $sql .=  " AND productName LIKE :productName";
+                  $namedParameters[":productName"] = "%" . $_GET['product'] . "%";
+             }
+             
              if (!empty($_GET['category'])) { //checks whether user has typed something in the "Product" text box
                  $sql .=  " AND catId = :categoryId";
-                 $namedParameters[":categoryId"] =  $_GET['category'];
-            }        
+                  $namedParameters[":categoryId"] =  $_GET['category'];
+             }        
             
-            //echo $sql; //for debugging purposes
+             if (!empty($_GET['priceFrom'])) { 
+                  $sql .=  " AND price >= :priceFrom";
+                  $namedParameters[":priceFrom"] =  $_GET['priceFrom'];
+             } 
             
-             $stmt = $conn->prepare($sql);
-             $stmt->execute($namedParameters);
-             $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-            foreach ($records as $record) {
-                echo "<a href=\"purchaseHistory.php?productId=".$record['productId']."\">" . $record['productName'] . " " . $record['productDescription'] . " $" . $record['price'] . "<br /><br /></a>";
-                //echo $record['productName'] . " " . $record['productDescription'] . " $" . $record['price'] . "<br /><br />";
-            }
-        }
-    }
-?>
+            if (!empty($_GET['priceTo'])) { 
+                 $sql .=  " AND price <= :priceTo";
+                 $namedParameters[":priceTo"] =  $_GET['priceTo'];
+             } 
+             
+             if (isset($_GET['orderBy'])) { 
+                  
+                  if($_GET['orderBy'] == "price"){
+                      $sql .=  " ORDER BY price";
+                  }
+                  else{
+                      $sql .=  " ORDER BY productName";
+                  }
+             } 
+             
+             //echo $sql; //for debugging purposes
+             
+              $stmt = $conn->prepare($sql);
+              $stmt->execute($namedParameters);
+              $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+         
+             foreach ($records as $record) {
+                 echo "<a href=\"purchaseHistory.php?productId=".$record['productId']."\">" . $record['productName'] . " " . $record['productDescription'] . " $" . $record['price'] . "<br /><br /></a>";
+                 //echo $record['productName'] . " " . $record['productDescription'] . " $" . $record['price'] . "<br /><br />";
+             }
+         }
+     }
+ ?>
 
 <!DOCTYPE html>
 <html>
