46c7ea1f6367c0631e74060ab41e46e6950a56fc
Sergio Llopis <sllopis@csumb.edu>
2018-05-04 18:42:24 +0000

    Buddy Badge Lab 10

--- labs/lab10/inc/footer.php
+++ labs/lab10/inc/footer.php
@@ -1,8 +1,8 @@
- 
-        <hr>
-        
-        <footer>
+<hr>
+
+<footer>
+        <img style="width:100px; text-align:center;" src="img/buddy_verified.png" alt="Buddy" />
         <strong>Disclaimer:</strong> All information in this website is fake.<br />
         &copy;2018 Lab 10 <br>
         Sergio Llopis
-        </footer>
+</footer>
